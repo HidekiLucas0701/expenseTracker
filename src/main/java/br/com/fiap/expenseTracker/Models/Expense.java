1c0ad11a15d9d8db3d92117520a2d89ca4c07862
HidekiLucas0701 <hideki.lucas0701@gmail.com>
2026-03-17 20:41:23 -0300

    feat(wip): expense entity

---
+++ src/main/java/br/com/fiap/expenseTracker/Models/Expense.java
@@ -0,0 +1,22 @@
+package br.com.fiap.expenseTracker.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private Double amount;
+    private LocalDate date;
+}
